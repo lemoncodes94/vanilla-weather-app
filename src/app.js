e83c74fb75bc69e5de8be501f088564f459acdaa
lemoncodes94 <lauren_mackay94@yahoo.co.uk>
2023-05-21 15:42:39 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -42,9 +42,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
+function search(city) {
+  let apiKey = "a5825o278b27ea6f2fde39eft437708c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-let apiKey = "a5825o278b27ea6f2fde39eft437708c";
-let city = "Glasgow";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
